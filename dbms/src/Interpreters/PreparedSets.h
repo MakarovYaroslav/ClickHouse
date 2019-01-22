f0e9964b2df29bd158aa95765f0833f8dbcf5cef
Alexey Zatelepin <ztlpn@yandex-team.ru>
2019-01-22 15:33:56 +0300

    add data types to the prepared_sets key

--- dbms/src/Interpreters/PreparedSets.h
+++ dbms/src/Interpreters/PreparedSets.h
@@ -1,20 +1,44 @@
 #pragma once
 
 #include <Parsers/IAST.h>
+#include <DataTypes/IDataType.h>
 #include <memory>
 #include <unordered_map>
 
 namespace DB
 {
 
-struct ASTHalfHash
+struct PreparedSetKey
 {
-    UInt64 operator()(const IAST::Hash & ast_hash) const { return ast_hash.first; }
+    IAST::Hash ast_hash;
+    DataTypes types; /// Empty for subqueries.
+
+    bool operator==(const PreparedSetKey & other) const
+    {
+        if (ast_hash != other.ast_hash)
+            return false;
+
+        if (types.size() != other.types.size())
+            return false;
+
+        for (size_t i = 0; i < types.size(); ++i)
+        {
+            if (!types[i]->equals(*other.types[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    struct Hash
+    {
+        UInt64 operator()(const PreparedSetKey & key) const { return key.ast_hash.first; }
+    };
 };
 
 class Set;
 using SetPtr = std::shared_ptr<Set>;
 
-using PreparedSets = std::unordered_map<IAST::Hash, SetPtr, ASTHalfHash>;
+using PreparedSets = std::unordered_map<PreparedSetKey, SetPtr, PreparedSetKey::Hash>;
 
 }
